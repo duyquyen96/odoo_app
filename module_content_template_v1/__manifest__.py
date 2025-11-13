7bba474c549b4b48e7f61f37fac5d32cdaeb461e
quyen <duyquyencnt55@gmail.com>
2025-11-13 09:53:00 +0700

    [add]

---
+++ module_content_template_v1/__manifest__.py
@@ -0,0 +1,52 @@
+{
+    'name': 'App preview index html v01',
+    'name_vi_VN': 'App preview index html v01',
+    'version': '0.1.0',
+    'category': 'Productivity',
+    'author': "T.V.T Marine Automation (aka TVTMA),Viindoo",
+    'website': 'https://viindoo.com/apps/app/16.0/to_web_thousand_sep',
+    'live_test_url': "https://v16demo-int.viindoo.com",
+    'live_test_url_vi_VN': "https://v16demo-vn.viindoo.com",
+    'demo_video_url': "https://youtu.be/gBVDAPoCOK8",
+    'support': 'apps.support@viindoo.com',
+    'sequence': 10,
+    'summary': 'Preview app index html',
+    'summary_vi_VN': 'Hiển thị dấu phân cách hàng nghìn ngay tại thời điểm nhập số liệu',
+    'description': """
+Demo video: `Web Thousand Separators <https://youtu.be/gBVDAPoCOK8>`_
+
+
+What it does
+============
+
+By default, the thousands separator only appears after you finish typing. This module shows separators on-fly during your input based on setting language of user.
+
+Editions Supported
+==================
+1. Community Edition
+2. Enterprise Edition
+    """,
+    'description_vi_VN': """
+Demo video: `Dấu phân cách hàng nghìn <https://youtu.be/gBVDAPoCOK8>`_
+
+
+Mô tả
+=====
+
+Mặc định, hệ thống chỉ hiển thị dấu phân cách hàng nghìn sau khi hoàn thành việc nhập số liệu. Mô-đun này giúp khắc phục vấn đề đó bằng cách hiển thị dấu phân cách ngay tại thời điểm nhập số liệu dựa trên thiết lập ngôn ngữ của người dùng.
+
+Ấn bản được hỗ trợ
+==================
+1. Ấn bản Community
+2. Ấn bản Enterprise
+    """,
+
+    'depends': ['web'],
+    'images': ['static/description/main_screenshot.png'],
+    'installable': True,
+    'auto_install': True,
+    'application': False,
+    'price': 18.9,
+    'currency': 'EUR',
+    'license': 'OPL-1',
+}
